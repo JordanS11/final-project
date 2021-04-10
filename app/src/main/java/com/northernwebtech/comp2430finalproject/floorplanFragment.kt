d305cc9a8e9fc3a103d0b13421f5ca5ef08f4e79
Jordan Scott <jscott11@lakeheadu.ca>
2021-04-09 22:26:15 -0400

    Floorplan Fragment Added
    Simple outline for the tables floor plan added. Fragment transaction also added.

--- app/src/main/java/com/northernwebtech/comp2430finalproject/floorplanFragment.kt
+++ app/src/main/java/com/northernwebtech/comp2430finalproject/floorplanFragment.kt
@@ -1,4 +1,8 @@
 package com.northernwebtech.comp2430finalproject
 
-class floorplanFragment {
+import androidx.fragment.app.Fragment
+
+class floorplanFragment : Fragment(R.layout.floorplan_layout) {
+
+
 }
